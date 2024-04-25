5f5b2eea510d19da04df6dea0c05e1d14539119b
Yoan <hivefr@gmail.com>
2024-04-25 10:41:26 +0200

    Offline Message

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+// .eslintrc.js
+
+module.exports = {
+  env: {
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  root: true,
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+};
--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
 import { Provider as PaperProvider } from "react-native-paper";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { NetworkProvider } from "react-native-offline";
 
 import { Header } from "./src/components/Header";
 import { LoginScreen } from "./src/screens/LoginScreen";
 import { TermsScreen } from "./src/screens/TermsScreen";
 import { StarshipFeedScreen } from "./src/screens/StarshipFeedScreen";
+import { ScreenContainer } from "./src/components/ScreenContainer";
 
 export default function App() {
   const queryClient = new QueryClient();
