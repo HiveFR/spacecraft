fde5a4b167bf1d2ccaf3568372e44734ce5571a3
Yoan <hivefr@gmail.com>
2024-04-25 10:43:25 +0200

    Fix ESLint errors

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
-import { StyleSheet, Text, View } from "react-native";
+// import { StyleSheet } from "react-native";
 import { Provider as PaperProvider } from "react-native-paper";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { NetworkProvider } from "react-native-offline";
+// import { NetworkProvider } from "react-native-offline";
 
 import { Header } from "./src/components/Header";
-import { LoginScreen } from "./src/screens/LoginScreen";
-import { TermsScreen } from "./src/screens/TermsScreen";
+// import { LoginScreen } from "./src/screens/LoginScreen";
+// import { TermsScreen } from "./src/screens/TermsScreen";
 import { StarshipFeedScreen } from "./src/screens/StarshipFeedScreen";
-import { ScreenContainer } from "./src/components/ScreenContainer";
+// import { ScreenContainer } from "./src/components/ScreenContainer";
 
 export default function App() {
   const queryClient = new QueryClient();
@@ -22,5 +22,3 @@ export default function App() {
     </QueryClientProvider>
   );
 }
-
-const styles = StyleSheet.create({});
